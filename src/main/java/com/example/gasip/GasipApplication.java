fbfb29e88df598f4512279405c056ce14d06ebe6
Harry <114489245+HyemIin@users.noreply.github.com>
2023-10-11 22:41:13 +0900

    Feat: Build Project

---
+++ src/main/java/com/example/gasip/GasipApplication.java
@@ -0,0 +1,13 @@
+package com.example.gasip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GasipApplication.class, args);
+    }
+
+}

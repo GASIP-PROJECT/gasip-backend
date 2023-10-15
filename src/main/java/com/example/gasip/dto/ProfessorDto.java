5cfc0ec1d8db8e3ae9c08a8b255fda53b39bc4a2
HyemIin <hyem5019@gmail.com>
2023-10-15 17:38:10 +0900

    Feat : ProfessorDto 추가

---
+++ src/main/java/com/example/gasip/dto/ProfessorDto.java
@@ -0,0 +1,27 @@
+package com.example.gasip.dto;
+import com.example.gasip.entity.ProfessorEntity;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ProfessorDto {
+
+    private Long professor_Id;
+    private Long major_Id;
+    private String major;
+    private String name;
+
+    public static ProfessorDto toProfessorDto(ProfessorEntity professorEntity) {
+        ProfessorDto professorDto = new ProfessorDto();
+        professorDto.setProfessor_Id(professorEntity.getProfessor_Id());
+        professorDto.setMajor_Id(professorEntity.getMajor_Id());
+        professorDto.setMajor(professorEntity.getMajor());
+        professorDto.setName(professorEntity.getName());
+
+        return professorDto;
+    }
+
+}

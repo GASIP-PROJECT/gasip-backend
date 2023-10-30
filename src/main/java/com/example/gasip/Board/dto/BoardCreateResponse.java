a429da16828a5e988e5dc1b39209148848a30c9f
HyemIin <hyem5019@gmail.com>
2023-10-30 13:40:38 +0900

    Feat: Board DTO 개발
    CRUD,Req,Res 구분하여 DTO 생성

---
+++ src/main/java/com/example/gasip/Board/dto/BoardCreateResponse.java
@@ -0,0 +1,41 @@
+package com.example.gasip.Board.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class BoardCreateResponse {
+
+    @NotNull
+    private Long postId;
+    @NotNull
+    private String content;
+    private Long clickCount;
+    private Long likeCount;
+    @NotNull
+    private LocalDateTime regDate;
+    @NotNull
+    private LocalDateTime updateDate;
+    @NotNull
+    private Long profId;
+
+    @Builder
+    public BoardCreateResponse(Long postId, String content, Long clickCount, Long likeCount, LocalDateTime regDate, LocalDateTime updateDate, Long profId) {
+        this.postId = postId;
+        this.content = content;
+        this.clickCount = clickCount;
+        this.likeCount = likeCount;
+        this.regDate = regDate;
+        this.updateDate = updateDate;
+        this.profId = profId;
+    }
+
+    //fromEntity메서드 개발
+
+
+}
---
+++ src/main/java/com/example/gasip/Board/dto/BoardReadResponse.java
@@ -0,0 +1,38 @@
+package com.example.gasip.Board.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class BoardReadResponse {
+    @NotNull
+    private Long postId;
+    @NotNull
+    private String content;
+    private Long clickCount;
+    private Long likeCount;
+    @NotNull
+    private LocalDateTime regDate;
+    @NotNull
+    private LocalDateTime updateDate;
+    @NotNull
+    private Long profId;
+
+    @Builder
+    public BoardReadResponse(Long postId, String content, Long clickCount, Long likeCount, LocalDateTime regDate, LocalDateTime updateDate, Long profId) {
+        this.postId = postId;
+        this.content = content;
+        this.clickCount = clickCount;
+        this.likeCount = likeCount;
+        this.regDate = regDate;
+        this.updateDate = updateDate;
+        this.profId = profId;
+    }
+
+    //fromEntity메서드 개발
+}

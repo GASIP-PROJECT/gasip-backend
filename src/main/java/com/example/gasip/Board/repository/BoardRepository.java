addbd541c07cb2a49e91df3cf09ed61024f208b1
Jihun Kim <zh1229@naver.com>
2023-11-03 22:53:40 +0900

    feat : BoardRepository.java 구현

--- src/main/java/com/example/gasip/Board/repository/BoardRepository.java
+++ src/main/java/com/example/gasip/Board/repository/BoardRepository.java
@@ -1,6 +1,11 @@
 package com.example.gasip.Board.repository;
 
 import com.example.gasip.Board.model.Board;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 

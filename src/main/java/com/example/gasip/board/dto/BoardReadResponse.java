4aed6f327ec965a3b095401b52ae6d634d89c1f1
HyemIin <hyem5019@gmail.com>
2023-11-09 16:34:26 +0900

    Fix: Read Board 기능 동작하도록 수정 - BoardReadResponse 변수 profId로 변경

--- src/main/java/com/example/gasip/board/dto/BoardReadResponse.java
+++ src/main/java/com/example/gasip/board/dto/BoardReadResponse.java
@@ -1,14 +1,11 @@
 package com.example.gasip.board.dto;
 
-import com.example.gasip.common.BaseTimeEntity;
 import com.example.gasip.board.model.Board;
-import com.example.gasip.professor.model.Professor;
+import com.example.gasip.common.BaseTimeEntity;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-
-import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
@@ -21,16 +18,7 @@ public class BoardReadResponse extends BaseTimeEntity {
     private Long clickCount;
     private Long likeCount;
     @NotNull
-    private Professor professor;
-
-    public BoardReadResponse(LocalDateTime regDate, LocalDateTime updateDate, Long postId, String content, Long clickCount, Long likeCount, Professor professor) {
-        super(regDate, updateDate);
-        this.postId = postId;
-        this.content = content;
-        this.clickCount = clickCount;
-        this.likeCount = likeCount;
-//        this.professor = professor;
-    }
+    private Long profId;
     public static BoardReadResponse fromEntity(Board board) {
         return BoardReadResponse.builder()
                 .regDate(board.getRegDate())
@@ -39,7 +27,7 @@ public class BoardReadResponse extends BaseTimeEntity {
                 .content(board.getContent())
                 .clickCount(board.getClickCount())
                 .likeCount(board.getLikeCount())
-//                .professor(board.getProfessor())
+                .profId(board.getProfessor().getProfId())
                 .build();
     }
 }

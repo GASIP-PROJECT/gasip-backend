575cc948a4e1ecbc91e647b6165dbe615995e07d
Jihun Kim <zh1229@naver.com>
2023-11-18 16:09:21 +0900

    Feat : Schema 설정  - 각 엔터티 별 스키마 설정 Fixes : #63

--- src/main/java/com/example/gasip/professor/dto/ProfessorDto.java
+++ src/main/java/com/example/gasip/professor/dto/ProfessorDto.java
@@ -1,19 +1,24 @@
 package com.example.gasip.professor.dto;
 
 import com.example.gasip.professor.model.Professor;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-
 @ToString
+@Schema(description = "교수 관련 VO")
 public class ProfessorDto {
 
+    @Schema(description = "교수 ID")
     private Long profId;
+    @Schema(description = "교수 전공 ID")
     private Long majorId;
+    @Schema(description = "교수 전공 이름")
     private String majorName;
+    @Schema(description = "교수 이름")
     private String profName;
 
     public static ProfessorDto toProfessorDto(Professor professorEntity) {

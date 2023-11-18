f57a68873d000775c2f6dad49fc1c54d5d97ad67
HyemIin <hyem5019@gmail.com>
2023-11-18 12:40:37 +0900

    Fix: 단과대/전공 엔티티 분리
    - Major 엔티티에 college 필드 삭제
    - College 도메인 생성
    - Major/College 1:1 연관관계 매핑

---
+++ src/main/java/com/example/gasip/college/model/College.java
@@ -0,0 +1,15 @@
+package com.example.gasip.college.model;
+
+import com.example.gasip.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class College extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long collegeId;
+    @Column(nullable = false)
+    private String collegeName;
+}

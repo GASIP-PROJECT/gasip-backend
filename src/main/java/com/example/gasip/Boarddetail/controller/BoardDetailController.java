4c94292567483a25483f7e266afbc3f8c5630c3b
Jihun Kim <zh1229@naver.com>
2023-11-18 15:58:30 +0900

    Feat : Operation 설정 - BoardDetailController의 Operation을 설정합니다. Fixes : #63

--- src/main/java/com/example/gasip/Boarddetail/controller/BoardDetailController.java
+++ src/main/java/com/example/gasip/Boarddetail/controller/BoardDetailController.java
@@ -2,6 +2,7 @@ package com.example.gasip.Boarddetail.controller;
 
 import com.example.gasip.Boarddetail.dto.BoardDetailResponse;
 import com.example.gasip.Boarddetail.service.BoardDetailService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ public class BoardDetailController {
     private final BoardDetailService boardDetailService;
 
     @GetMapping("/{postId}")
+    @Operation(summary = "게시글 상세 정보 불러오기", description = "게시글 상세 정보를 불러옵니다.", tags = { "BoardDetail Controller" })
     public ResponseEntity<BoardDetailResponse> getBoardDetail(@PathVariable Long postId) {
         return ResponseEntity.ok(boardDetailService.findByID(postId));
     }

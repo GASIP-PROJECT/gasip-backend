125b4346df1c43e01eb4df4f2c2ba3c2ffde7dba
Jihun Kim <zh1229@naver.com>
2023-11-18 15:58:57 +0900

    Feat : Operation 설정 - ProfessorController Operation을 설정합니다. Fixes : #63

--- src/main/java/com/example/gasip/professor/controller/ProfessorController.java
+++ src/main/java/com/example/gasip/professor/controller/ProfessorController.java
@@ -2,6 +2,7 @@ package com.example.gasip.professor.controller;
 
 import com.example.gasip.professor.dto.ProfessorResponse;
 import com.example.gasip.professor.service.ProfessorService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,11 +20,13 @@ public class ProfessorController {
     private final ProfessorService professorService;
 
     @GetMapping("")
+    @Operation(summary = "교수 전체 목록 불러오기", description = "교수 전체 목록을 불러옵니다.", tags = { "Professor Controller" })
     public ResponseEntity<List<ProfessorResponse>> findAllProfessor() {
         return ResponseEntity.ok(professorService.findAll());
     }
 
     @GetMapping("{profId}")
+    @Operation(summary = "교수 상세 정보 불러오기", description = "교수 상세 정보를 불러옵니다.", tags = { "Professor Controller" })
     public ResponseEntity<ProfessorResponse> findByProfId(@PathVariable Long profId) {
         return ResponseEntity.ok(professorService.findByProfId(profId));
     }

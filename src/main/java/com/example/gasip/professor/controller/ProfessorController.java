1b10f8634fed6f838271fc4c0e041c3ece740d90
Jihun Kim <zh1229@naver.com>
2023-11-18 16:31:14 +0900

    Feat : ApiResponse 설정  - ApiResponse 설정  - 200, 400, 404, 500 관련 API 응답 설정 Fixes : #63

--- src/main/java/com/example/gasip/professor/controller/ProfessorController.java
+++ src/main/java/com/example/gasip/professor/controller/ProfessorController.java
@@ -3,6 +3,10 @@ package com.example.gasip.professor.controller;
 import com.example.gasip.professor.dto.ProfessorResponse;
 import com.example.gasip.professor.service.ProfessorService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +25,13 @@ public class ProfessorController {
 
     @GetMapping("")
     @Operation(summary = "교수 전체 목록 불러오기", description = "교수 전체 목록을 불러옵니다.", tags = { "Professor Controller" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(schema = @Schema(implementation = ResponseEntity.class))),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     public ResponseEntity<List<ProfessorResponse>> findAllProfessor() {
         return ResponseEntity.ok(professorService.findAll());
     }

bb8d2983c9f9bf72dc54e75f5747184523167226
Jihun Kim <zh1229@naver.com>
2023-11-21 13:12:56 +0900

    feat : College Read 구현 - Service 구현 Resolves: #69

---
+++ src/main/java/com/example/gasip/college/dto/CollegeResponse.java
@@ -0,0 +1,36 @@
+package com.example.gasip.college.dto;
+
+import com.example.gasip.college.model.College;
+import com.example.gasip.common.BaseTimeEntity;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@SuperBuilder
+public class CollegeResponse extends BaseTimeEntity {
+
+    @NotNull
+    private Long collegeId;
+    @NotNull
+    private String collegeName;
+
+    public CollegeResponse(Long collegeId, LocalDateTime regDate, LocalDateTime updateDate, String collegeName) {
+        super(regDate, updateDate);
+        this.collegeId = collegeId;
+        this.collegeName = collegeName;
+    }
+
+    public static CollegeResponse fromEntity(College college) {
+        return CollegeResponse.builder()
+                .regDate(college.getRegDate())
+                .updateDate(college.getUpdateDate())
+                .collegeId(college.getCollegeId())
+                .collegeName(college.getCollegeName())
+                .build();
+    }
+}

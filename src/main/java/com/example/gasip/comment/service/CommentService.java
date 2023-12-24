42f2679bd4cedff60ce91ceb05cf9ea11e092b73
HyemIin <hyem5019@gmail.com>
2023-12-24 23:30:45 +0900

    Refactor: Comment create 리팩토링
    - api 공통 포맷 적용
    - create 기능 작성

--- src/main/java/com/example/gasip/comment/service/CommentService.java
+++ src/main/java/com/example/gasip/comment/service/CommentService.java
@@ -1,19 +1,18 @@
 package com.example.gasip.comment.service;
 
-import com.example.gasip.board.dto.BoardReadResponse;
 import com.example.gasip.board.model.Board;
 import com.example.gasip.board.repository.BoardRepository;
 import com.example.gasip.comment.dto.CommentCreateRequest;
 import com.example.gasip.comment.dto.CommentDto;
 import com.example.gasip.comment.model.Comment;
 import com.example.gasip.comment.repository.CommentRepository;
-import com.example.gasip.exception.BoardNotFoundException;
-import com.example.gasip.exception.CommentNotFoundException;
+import com.example.gasip.global.security.MemberDetails;
+import com.example.gasip.member.model.Member;
+import com.example.gasip.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,46 +21,48 @@ import java.util.stream.Collectors;
 public class CommentService {
     private final CommentRepository commentRepository;
     private final BoardRepository boardRepository;
+    private final MemberRepository memberRepository;
 
     // 댓글 목록 전체 조회
     @Transactional(readOnly = true)
     public List<CommentDto> CommentList() {
-//        List<CommentDto> commentCreateResponseList = commentRepository.findComment().stream().map(CommentDto::fromEntity).collect(Collectors.toList());
-        List<Comment> comments = commentRepository.findComment();
-        List<CommentDto> commentCreateResponseList = new ArrayList<>();
-        for(Comment comment : comments) {
-            commentCreateResponseList.add(CommentDto.ToEntity(comment));
-        }
-        return commentCreateResponseList;
+        return commentRepository.findComment()
+            .stream()
+            .map(CommentDto::fromEntity)
+            .collect(Collectors.toList());
     }
 
     // 특정 게시글 댓글 조회
     @Transactional(readOnly = true)
     public List<CommentDto> findCommentByBoard(Board postId) {
-        List<Comment> comments = commentRepository.findCommentByBoard(postId);
-        List<CommentDto> findCommentByBoard = new ArrayList<>();
-        for(Comment comment : comments) {
-            findCommentByBoard.add(CommentDto.ToEntity(comment));
-        }
-        return findCommentByBoard;
+        return commentRepository.findCommentByBoard(postId)
+            .stream()
+            .map(CommentDto::fromEntity)
+            .collect(Collectors.toList());
     }
 
     // 댓글 create
     @Transactional
-    public CommentDto createComment(CommentCreateRequest commentCreateRequest) {
-        Comment comment = commentRepository.save(
-                Comment.createComment(
-                        commentCreateRequest.getContent(),
-                        boardRepository.findById(commentCreateRequest.getPostId()).orElseThrow(BoardNotFoundException::new),
-                        commentCreateRequest.getWriter(),
-                        commentCreateRequest.getParentId() != null ?
-                                commentRepository.findById(commentCreateRequest.getParentId()).orElseThrow(CommentNotFoundException::new) : null)
-        );
+    public CommentDto createComment(MemberDetails memberDetails, CommentCreateRequest commentCreateRequest, Long boardId) {
+        Member member = memberRepository.getReferenceById(memberDetails.getId());
+        Board board = boardRepository.findById(boardId).orElseThrow(IllegalArgumentException::new);
+        Comment comment = commentRepository.save(commentCreateRequest.toEntity(board,member));
 
-//                Comment.createComment(commentCreateRequest.getContent(),
+        Comment parentComment;
+        if (commentCreateRequest.getParentId() != null) {
+            parentComment = commentRepository.findById(commentCreateRequest.getParentId()).orElseThrow(IllegalArgumentException::new);
+            comment.updateParent(parentComment);
+        }
+//        Comment comment = commentRepository.save(
+//                Comment.createComment(
+//                commentCreateRequest.getContent(),
+//                boardRepository.findById(commentCreateRequest.getPostId()).orElseThrow(BoardNotFoundException::new),
+//                commentCreateRequest.getWriter(),
 //                commentCreateRequest.getParentId() != null ?
-//                        commentRepository.findById(commentCreateRequest.getParentId()).orElseThrow(CommentNotFoundException::new) : null);
-        return CommentDto.ToEntity(comment);
+//                    commentRepository.findById(commentCreateRequest.getParentId()).orElseThrow(CommentNotFoundException::new) : null)
+//        );
+
+        return CommentDto.fromEntity(comment);
     }
 
     // 댓글 delete

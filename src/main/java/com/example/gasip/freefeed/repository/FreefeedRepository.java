25aa683f12217436af02b40b7a1edfdd0cb424eb
HyemIin <hyem5019@gmail.com>
2024-03-09 22:44:31 +0900

    Feat : FreeFeed 도메인 삭제
    - Board 도메인에서 통합하여 진행할 수 있도록 결정

--- src/main/java/com/example/gasip/freefeed/repository/FreefeedRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.gasip.freefeed.repository;
-
-import com.example.gasip.freefeed.model.Freefeed;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FreefeedRepository extends JpaRepository<Freefeed,Long> {
-}

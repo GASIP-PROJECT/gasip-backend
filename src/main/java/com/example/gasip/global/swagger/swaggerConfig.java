3a33718e77e7b39973ad600698825660f16204f4
Jihun Kim <zh1229@naver.com>
2024-03-11 14:16:58 +0900

    feat : swaggerConfig.java 코드에 JWT 기능 사용할 수 있게 설정 추가 Resolves: #129

--- src/main/java/com/example/gasip/global/swagger/swaggerConfig.java
+++ src/main/java/com/example/gasip/global/swagger/swaggerConfig.java
@@ -2,10 +2,16 @@ package com.example.gasip.global.swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @OpenAPIDefinition(
         info = @Info(title = "Gasip 서비스 API 명세서",
@@ -27,6 +33,23 @@ public class swaggerConfig {
                 .pathsToMatch(paths)
                 .build();
     }
+
+    /**
+     * JWT 관련 설정
+     */
+    @Bean
+    public OpenAPI openAPI(){
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(Arrays.asList(securityRequirement));
+    }
+
 
     /**
      * 교수 정보 그룹

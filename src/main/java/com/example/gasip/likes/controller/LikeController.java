6dbd329596f005215ae3bd00ebc9896f710764b7
Jihun Kim <zh1229@naver.com>
2024-03-26 16:21:14 +0900

    feat : 부하테스트를 위한 addLikeWithoutMember 추가
    Resolves: #176

--- src/main/java/com/example/gasip/likes/controller/LikeController.java
+++ src/main/java/com/example/gasip/likes/controller/LikeController.java
@@ -1,11 +1,14 @@
 package com.example.gasip.likes.controller;
 
+import com.example.gasip.global.api.ApiUtils;
 import com.example.gasip.global.entity.HttpResponseEntity;
+import com.example.gasip.global.security.MemberDetails;
 import com.example.gasip.likes.dto.LikeRequestDto;
 import com.example.gasip.likes.service.LikeService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import static com.example.gasip.global.entity.HttpResponseEntity.success;
@@ -18,14 +21,18 @@ public class LikeController {
     private final LikeService likeService;
 
     @PostMapping
-    public HttpResponseEntity.ResponseResult<?> insert(@RequestBody @Valid LikeRequestDto likeRequestDto) throws Exception {
-        likeService.insert(likeRequestDto);
+    public HttpResponseEntity.ResponseResult<?> insert(@RequestBody @Valid LikeRequestDto likeRequestDto,
+                                                       @AuthenticationPrincipal MemberDetails memberDetails) throws Exception {
+//        likeService.insert(likeRequestDto, memberDetails);
+        // API 부하테스트용
+        ApiUtils.success(likeService.addLikeWithoutMember(likeRequestDto));
         return success();
     }
 
     @DeleteMapping
-    public HttpResponseEntity.ResponseResult<?> delete(@RequestBody @Valid LikeRequestDto likeRequestDto) {
-        likeService.delete(likeRequestDto);
+    public HttpResponseEntity.ResponseResult<?> delete(@RequestBody @Valid LikeRequestDto likeRequestDto,
+                                                       @AuthenticationPrincipal MemberDetails memberDetails) {
+        likeService.delete(likeRequestDto, memberDetails);
         return success();
     }
 }

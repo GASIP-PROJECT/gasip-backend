c1d8e5202a2881032815a2bf96a522afd0e418c1
Jihun Kim <zh1229@naver.com>
2024-03-27 22:25:31 +0900

    feat : Major, college, Category 도메인 병합
    Resolves: #176

--- src/main/java/com/example/gasip/professor/controller/ProfessorController.java
+++ src/main/java/com/example/gasip/professor/controller/ProfessorController.java
@@ -62,14 +62,14 @@ public class ProfessorController {
     /**
      * 특정 학과 교수
      */
-    @GetMapping("/majors/{majorId}")
+    @GetMapping("/majors/{Id}")
     @Operation(summary = "학과별 교수 정보 불러오기", description = "학과별 교수 정보를 불러옵니다.", tags = { "Professor Controller" })
-    public ResponseEntity<?> findAllById(@PathVariable Category majorId) {
+    public ResponseEntity<?> findAllById(@PathVariable Category Id) {
         return ResponseEntity
                 .ok()
                 .body(
                         ApiUtils.success(
-                                professorService.findProfByMajor(majorId)
+                                professorService.findProfByMajor(Id)
                         )
                 );
     }

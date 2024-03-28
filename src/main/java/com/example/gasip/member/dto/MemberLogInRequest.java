be20b879fbe5860a5455b587285ff735fad64515
Jihun Kim <zh1229@naver.com>
2024-03-28 19:24:23 +0900

    feat : 로그인 관련 스키마 정보 작성
    Resolves: #63

--- src/main/java/com/example/gasip/member/dto/MemberLogInRequest.java
+++ src/main/java/com/example/gasip/member/dto/MemberLogInRequest.java
@@ -1,5 +1,6 @@
 package com.example.gasip.member.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,7 +8,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MemberLogInRequest {
 
+    @Schema(description = "이메일", example = "abc@gmail.com")
     private String email;
-
+    @Schema(description = "비밀번호", example = "1234!@")
     private String password;
 }

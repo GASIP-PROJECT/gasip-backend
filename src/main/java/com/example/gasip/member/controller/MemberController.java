f45a97846d8c434f329c5e49109d4f6c6cecb6a2
Jihun Kim <zh1229@naver.com>
2024-03-28 19:25:11 +0900

    feat : SignUp, SignIn Operation, ApiResponse 작성
    Resolves: #63

--- src/main/java/com/example/gasip/member/controller/MemberController.java
+++ src/main/java/com/example/gasip/member/controller/MemberController.java
@@ -2,8 +2,17 @@ package com.example.gasip.member.controller;
 
 import com.example.gasip.global.api.ApiUtils;
 import com.example.gasip.global.security.MemberDetails;
-import com.example.gasip.member.dto.*;
+import com.example.gasip.member.dto.MemberLogInRequest;
+import com.example.gasip.member.dto.MemberLogInResponse;
+import com.example.gasip.member.dto.MemberSignUpRequest;
+import com.example.gasip.member.dto.MemberSignUpResponse;
 import com.example.gasip.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,6 +20,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Member Controller", description = "교수 평가 서비스를 제공하는 Gasip을 사용하기 위한 토큰 발급 및 인증 프로세스를 제공합니다.")
 @RestController
 @RequestMapping("/members")
 @RequiredArgsConstructor
@@ -19,6 +29,14 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
+    @Operation(summary = "계정 생성", description = "Gasip 서비스를 이용하기 위해서는 인가 토큰을 발급받아야 합니다. \n" +
+            "인가 토큰을 발급을 위해서 Gasip 서비스에 사용할 계정을 가입해야 합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Created",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = MemberSignUpResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     public ResponseEntity<?> signup(@RequestBody @Valid MemberSignUpRequest memberSignUpRequest) {
         return ResponseEntity
             .status(HttpStatus.CREATED)
@@ -30,6 +48,14 @@ public class MemberController {
     }
 
     @PostMapping("/login")
+    @Operation(summary = "로그인 토큰 인증 및 발급", description = "Gasip에서 계정 생성을 한 이후 토큰 발급을 수행해야 합니다. 토큰 발급은 "
+            + "로그인을 통해 진행하게 됩니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = MemberLogInResponse.class))}),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
     public ResponseEntity<?> login(@RequestBody @Valid MemberLogInRequest memberLogInRequest) {
         return ResponseEntity
             .ok()

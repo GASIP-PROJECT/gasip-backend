9f5ecee809023ef467fe786599b158d43820d655
HyemIin <hyem5019@gmail.com>
2024-03-29 18:05:45 +0900

    Feat : 평점 수정 api 개발
    - 사용자가 기존 등록한 평점을 수정할 수 있는 로직 개발
    - 평점 Read는 비로그인 사용자도 읽을 수 있도록 설정

--- src/main/java/com/example/gasip/grade/service/GradeService.java
+++ src/main/java/com/example/gasip/grade/service/GradeService.java
@@ -2,17 +2,19 @@ package com.example.gasip.grade.service;
 
 import com.example.gasip.global.security.MemberDetails;
 import com.example.gasip.grade.dto.request.GradeCreateRequest;
+import com.example.gasip.grade.dto.request.GradeUpdateRequest;
 import com.example.gasip.grade.dto.response.GradeCreateResponse;
 import com.example.gasip.grade.dto.response.GradeGetDto;
+import com.example.gasip.grade.dto.response.GradeUpdateResponse;
 import com.example.gasip.grade.model.Grade;
 import com.example.gasip.grade.repository.GradeRepository;
-import com.example.gasip.grade.repository.GradeRepositoryCustom;
 import com.example.gasip.member.model.Member;
 import com.example.gasip.member.repository.MemberRepository;
 import com.example.gasip.professor.model.Professor;
 import com.example.gasip.professor.repository.ProfessorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -22,6 +24,7 @@ public class GradeService {
     private final GradeRepository gradeRepository;
     private final ProfessorRepository professorRepository;
     private final MemberRepository memberRepository;
+    @Transactional
     public GradeCreateResponse createGrade(GradeCreateRequest gradeCreateRequest, MemberDetails memberDetails, Long profId) {
         Professor professor = professorRepository.getReferenceById(profId);
         Member member = memberRepository.getReferenceById(memberDetails.getId());
@@ -35,8 +38,19 @@ public class GradeService {
         return GradeCreateResponse.fromEntity(grade);
     }
 
-
+    @Transactional(readOnly = true)
     public List<GradeGetDto> findProfessorGradepoint(Long profId) {
         return gradeRepository.professorAverageGradepoint(profId);
+    }
+    @Transactional
+    public GradeUpdateResponse updateProfessorGradepoint(MemberDetails memberDetails, GradeUpdateRequest gradeUpdateRequest, Long profId) {
+        Professor professor = professorRepository.getReferenceById(profId);
+        Member member = memberRepository.getReferenceById(memberDetails.getId());
+        if (5 < gradeUpdateRequest.getGradepoint() || 0 > gradeUpdateRequest.getGradepoint()) {
+            throw new IllegalArgumentException("평점은 0~5점 사이로 입력해주세요.");
+        }
+        Grade grade = gradeRepository.findByProfessorAndMember(professor, member);
+        grade.updateGradepoint(gradeUpdateRequest.getGradepoint());
+        return GradeUpdateResponse.fromEntity(grade);
     }
 }

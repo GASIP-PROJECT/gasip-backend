bfc8db0ffc49146e5e7be04d7dea7cf0a9a186a0
Jihun Kim <zh1229@naver.com>
2024-04-10 23:38:01 +0900

    feat : 전체 게시글 내림차순으로 호출하게 변경
    Resolves: #224

--- src/main/java/com/example/gasip/board/repository/BoardRepository.java
+++ src/main/java/com/example/gasip/board/repository/BoardRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface BoardRepository extends JpaRepository<Board,Long>,BoardRepositoryCustom {
     Page<Board> findAllByOrderByRegDateDesc(Pageable pageable);
@@ -16,5 +14,5 @@ public interface BoardRepository extends JpaRepository<Board,Long>,BoardReposito
     // 게시글 내용 검색
     Page<Board> findByContentContaining(String content, Pageable pageable);
 
-    Page<Board> findAllByProfessor(Professor professor, Pageable pageable);
+    Page<Board> findAllByProfessorOrderByRegDateDesc(Professor professor, Pageable pageable);
 }

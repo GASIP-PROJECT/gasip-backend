67010d7dcd9fe2dea4225d4fd50a1d69cdf78495
Jihun Kim <zh1229@naver.com>
2024-04-19 23:07:43 +0900

    Refactor : 좋아요 중복 요청 시 및 409에러 반환
    Resolves: #277

--- src/main/java/com/example/gasip/global/constant/ErrorCode.java
+++ src/main/java/com/example/gasip/global/constant/ErrorCode.java
@@ -2,44 +2,47 @@ package com.example.gasip.global.constant;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
 
     // 기존 Exception 에 대한 ErrorCode
-    NO_HANDLER_FOUND("존재하지 않는 URL"),
-    HTTP_REQUEST_METHOD_NOT_SUPPORTED("지원되지 않는 요청 메소드"),
+    NO_HANDLER_FOUND(HttpStatus.NOT_FOUND ,"존재하지 않는 URL"),
+    HTTP_REQUEST_METHOD_NOT_SUPPORTED(HttpStatus.METHOD_NOT_ALLOWED, "지원되지 않는 요청 메소드"),
 
-    METHOD_ARGUMENT_TYPE_MISMATCH("요청 파라미터 오류"),
-    PARAM_PARSING_MAPPING_ERROR("요청 입력 파싱 또는 매핑 오류"),
-    BEAN_VALIDATION_ERROR("입력 데이터 유효성 검증 오류"),
+    METHOD_ARGUMENT_TYPE_MISMATCH(HttpStatus.BAD_REQUEST, "요청 파라미터 오류"),
+    PARAM_PARSING_MAPPING_ERROR(HttpStatus.BAD_REQUEST, "요청 입력 파싱 또는 매핑 오류"),
+    BEAN_VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "입력 데이터 유효성 검증 오류"),
 
-    INTERNAL_SERVER_ERROR("Internal Server Error"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"Internal Server Error"),
 
-    UNAUTHORIZED("인증 오류"),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증 오류"),
 
     // Custom Exception 에 대한 ErrorCode
-    NO_SUCH_ENTITY("존재하지 않는 엔티티"),
-    INVALID_PARAM_ERROR("유효하지 않은 데이터"),
-    DUPLICATE_EMAIL("이미 존재하는 이메일"),
-    ACCESS_FORBIDDEN("권한 오류"),
-    DUPLICATE_LIKE("중복 좋아요 오류"),
-    NO_STOCK_ERROR("재고 부족 오류"),
-    DUPLICATE_PHONENUMBER("이미 존재하는 휴대폰 번호"),
+    NO_SUCH_ENTITY(HttpStatus.BAD_REQUEST, "존재하지 않는 엔티티"),
+    INVALID_PARAM_ERROR(HttpStatus.BAD_REQUEST, "유효하지 않은 데이터"),
+    DUPLICATE_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일"),
+    ACCESS_FORBIDDEN(HttpStatus.UNAUTHORIZED, "권한 오류"),
+    DUPLICATE_LIKE(HttpStatus.CONFLICT, "좋아요 중복 오류"),
+//    NO_STOCK_ERROR("재고 부족 오류"),
+    DUPLICATE_PHONENUMBER(HttpStatus.CONFLICT, "이미 존재하는 휴대폰 번호"),
 
     // Member
-    NOT_FOUND_MEMBER("회원 정보가 없습니다. 로그인 후 이용해주세요."),
-    DUPLICATE_MEMBER("이미 등록된 회원입니다."),
+    NOT_FOUND_MEMBER(HttpStatus.BAD_REQUEST, "회원 정보가 없습니다. 로그인 후 이용해주세요."),
+    DUPLICATE_MEMBER(HttpStatus.CONFLICT, "이미 등록된 회원입니다."),
     // Professor
-    NOT_FOUND_PROFESSOR("교수 정보 조회 불가 오류"),
+    NOT_FOUND_PROFESSOR(HttpStatus.NOT_FOUND, "교수 정보 조회 불가 오류"),
     //Board
-    INVALID_WRITTER("작성자만 글을 수정하거나 삭제할 수 있습니다."),
-    NOT_FOUND_BOARD("해당 게시글을 찾을 수 없습니다."),
+    INVALID_WRITTER(HttpStatus.UNAUTHORIZED, "작성자만 글을 수정하거나 삭제할 수 있습니다."),
+    NOT_FOUND_BOARD(HttpStatus.NOT_FOUND,"해당 게시글을 찾을 수 없습니다."),
 
     // Grade
-    DUPLICATE_GRADE("이미 평점을 등록했습니다."),
-    INVALID_GRADEPOINT("평점은 0~5점 사이로 입력해주세요.");
+    DUPLICATE_GRADE(HttpStatus.CONFLICT,"이미 평점을 등록했습니다."),
+    INVALID_GRADEPOINT(HttpStatus.BAD_REQUEST, "평점은 0~5점 사이로 입력해주세요.");
 
+    private final org.springframework.http.HttpStatus httpStatus;
     private final String message;
+
 }

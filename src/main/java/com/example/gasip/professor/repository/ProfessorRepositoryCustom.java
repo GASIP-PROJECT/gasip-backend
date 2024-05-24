9f65b537fdfba3f79101966ee5a303e71f6fcc72
Jihun Kim <zh1229@naver.com>
2024-05-24 12:39:45 +0900

    feat : 추가 findProfessorByCategoryNameContaining   majorName 키워드로 교수 목록 검색 Resolves: #320

--- src/main/java/com/example/gasip/professor/repository/ProfessorRepositoryCustom.java
+++ src/main/java/com/example/gasip/professor/repository/ProfessorRepositoryCustom.java
@@ -1,10 +1,13 @@
 package com.example.gasip.professor.repository;
 
+import com.example.gasip.professor.dto.ProfessorResponse;
 import com.example.gasip.professor.dto.ProfessorWithBoardResponse;
 
 import java.util.List;
 
 public interface ProfessorRepositoryCustom {
     List<ProfessorWithBoardResponse> findBoardByProfessor(Long profId);
+
+    List<ProfessorResponse> findProfessorByCategoryNameContaining(String majorName);
 }
 
--- src/main/java/com/example/gasip/professor/repository/ProfessorRepositoryCustomImpl.java
+++ src/main/java/com/example/gasip/professor/repository/ProfessorRepositoryCustomImpl.java
@@ -1,7 +1,9 @@
 package com.example.gasip.professor.repository;
 
+import com.example.gasip.professor.dto.ProfessorResponse;
 import com.example.gasip.professor.dto.ProfessorWithBoardResponse;
 import com.example.gasip.professor.dto.QProfessorWithBoardResponse;
+import com.example.gasip.professor.dto.QProfessorResponse;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
@@ -20,6 +22,17 @@ public class ProfessorRepositoryCustomImpl implements ProfessorRepositoryCustom{
                         professor.profId, professor.profName, professor.category.Id, professor.category.majorName, board.content, board.postId, board.member.nickname))
                 .from(professor, board)
                 .where(professor.profId.eq(board.professor.profId), board.professor.profId.eq(profId))
+                .fetch();
+    }
+
+    // 학과 키워드를 통한 교수 검색
+    @Override
+    public List<ProfessorResponse> findProfessorByCategoryNameContaining(String majorName) {
+        return queryFactory
+                .select(new QProfessorResponse(
+                        professor.profId, professor.profName, professor.category.collegeName, professor.category.Id, professor.category.majorName))
+                .from(professor)
+                .where(professor.category.majorName.eq(majorName))
                 .fetch();
     }
 }

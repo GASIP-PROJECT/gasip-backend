add2b627c05649effbfbbd6439da6ecd34656ff2
HyemIin <hyem5019@gmail.com>
2024-07-04 21:31:09 +0900

    HotFix : 평균 평점 로직 수정
    - null인 경우 평균평점이 0으로 나오도록 수정

--- src/main/java/com/example/gasip/professor/service/ProfessorService.java
+++ src/main/java/com/example/gasip/professor/service/ProfessorService.java
@@ -105,7 +105,7 @@ public class ProfessorService {
      */
     @Transactional
     public List<ProfessorResponse> findProfessorByCategoryNameContaining(String majorName, MemberDetails memberDetails) {
-        long beforeTime = System.currentTimeMillis();
+
         Member member = memberRepository.findById(memberDetails.getId()).orElseThrow(
             () -> new MemberNotFoundException(ErrorCode.NOT_FOUND_MEMBER)
         );
@@ -122,9 +122,6 @@ public class ProfessorService {
             }
             professorResponses.add(ProfessorResponse.fromEntity(professor));
         }
-        long afterTime = System.currentTimeMillis(); // 코드 실행 후에 시간 받아오기
-        long secDiffTime = (afterTime - beforeTime)/1000; //두 시간에 차 계산
-        System.out.println("시간차이(m) : "+secDiffTime);
         return professorResponses;
     }
 

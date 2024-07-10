587392542a2f3f5b1bd05dd892eb3a4cbe81cdd5
HyemIin <hyem5019@gmail.com>
2024-07-10 23:05:11 +0900

    Feat : 알림 스레드 풀 작업 큐 변경
    - LinkedBlockingQueue로 변경

--- src/main/java/com/example/gasip/global/config/AsyncConfig.java
+++ src/main/java/com/example/gasip/global/config/AsyncConfig.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
-import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 
 @EnableAsync
@@ -19,13 +19,13 @@ public class AsyncConfig implements AsyncConfigurer {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor() {
             @Override
             protected java.util.concurrent.BlockingQueue<Runnable> createQueue(int queueCapacity) {
-                return new SynchronousQueue<>();
+                return new LinkedBlockingQueue<>();
             }
         };
 
         executor.setCorePoolSize(4);
         executor.setMaxPoolSize(4);
-        executor.setQueueCapacity(0);  // SynchronousQueue 이므로 0으로 설정
+        executor.setQueueCapacity(Integer.MAX_VALUE);  // SynchronousQueue 이므로 0으로 설정
         executor.setKeepAliveSeconds(30);
         executor.setThreadNamePrefix("async-executor-");
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());

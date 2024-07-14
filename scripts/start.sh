c5b9366e7e5bb4b244604e2b85509883e98041a1
Jihun Kim <zh1229@naver.com>
2024-07-14 14:42:12 +0900

    fix : 변경 jar 파일 실행 추가 Resolves: #385

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,11 +9,15 @@ START_LOG="$ROOT_PATH/start.log"
 
 NOW=$(date +%c)
 
-echo "[$NOW] $JAR 복사" >> $START_LOG
-cp $ROOT_PATH/build/libs/spring-github-action-1.0.0.jar $JAR
+#echo "[$NOW] $JAR 복사" >> $START_LOG
+#cp $ROOT_PATH/build/libs/spring-github-action-1.0.0.jar $JAR
 
+#nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
 echo "[$NOW] > $JAR 실행" >> $START_LOG
-nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
+JAR_NAME=$(ls -tr $ROOT_PATH/ | grep SNAPSHOT.jar | tail -n 1)
 
-SERVICE_PID=$(pgrep -f $JAR)
-echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
+echo "> Jar Name: $JAR_NAME"
+nohup ./run.sh &
+
+#SERVICE_PID=$(pgrep -f $JAR)
+#echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
